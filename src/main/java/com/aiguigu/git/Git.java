16b7e031c52354d825958e2b63b8fef272afb160
wzw <1625547198@qq.com>
2022-07-01 17:13:01 +0800

    second commit

--- src/main/java/com/aiguigu/git/Git.java
+++ src/main/java/com/aiguigu/git/Git.java
@@ -3,5 +3,6 @@ package com.aiguigu.git;
 public class Git {
     public static void main(String[] args) {
         System.out.println("hell git");
+        System.out.println("hell git2");
     }
 }
